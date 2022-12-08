7f8c8f08a3193a453d01a4fcbf7ee05dda8945fd
lichao <184078718@qq.com>
2022-12-08 18:16:01 +0800

    生成脚手架

--- bin/ms-cli.js
+++ bin/ms-cli.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
 
 // 将构建目录(lib)下的 index.js 作为脚手架的入口
-require('../lib/index')
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+require('../lib/index');
